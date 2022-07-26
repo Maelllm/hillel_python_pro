2d5f7cc80c1075acfc7e2d3b3b5894d93d230d31
Maelllm <maelllm@gmail.com>
2022-07-26 22:55:09 +0300

    task_6 complete

---
+++ task_6/tests/test_6.py
@@ -0,0 +1,14 @@
+from task_6.task_6_1 import frange
+
+assert(list(frange(5)) == [0, 1, 2, 3, 4])
+assert(list(frange(2, 5)) == [2, 3, 4])
+assert(list(frange(2, 10, 2)) == [2, 4, 6, 8])
+assert(list(frange(10, 2, -2)) == [10, 8, 6, 4])
+assert(list(frange(2, 5.5, 1.5)) == [2, 3.5, 5])
+assert(list(frange(1, 5)) == [1, 2, 3, 4])
+assert(list(frange(0, 5)) == [0, 1, 2, 3, 4])
+assert(list(frange(0, 0)) == [])
+assert(list(frange(100, 0)) == [])
+
+print('SUCCESS!')
+
